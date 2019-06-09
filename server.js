4bd68b68f57524a16d3e3bda551e80c33fed3c23
yuderandre <palenciayudercy@gmail.com>
2019-06-09 13:30:30 -0400

    validacion de parametros

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', function (req, res) {
 app.get('/image/list', async function (req, res) {
     
     let { textSearch, limitImages } = req.query
+    if (!textSearch){
+        res.status(400).json({error:400,messageError:"textSearch no existe"})
+    }
+    if (!limitImages||limitImages<3||limitImages>200){
+        res.status(400).json({error:400,messageError:"limitImages no existe o es menor que 3 o mayor que 200"})
+    }
     
     let urlSearch = `https://pixabay.com/api/?key=12664246-2d4d93d2fe8f4ce834876a53d&q=${textSearch}&image_type=photo&per_page=${limitImages}`
     const resultImages = await getImage(urlSearch)

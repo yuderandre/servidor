c8819267927964f781279393ffe011f69b43474e
yuderandre <palenciayudercy@gmail.com>
2019-06-09 13:38:46 -0400

    se aplico map para crear arreglos de imagenes

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.get('/', function (req, res) {
     res.send('bienvenido')
 })
 app.get('/image/list', async function (req, res) {
-    
+
     let { textSearch, limitImages } = req.query
-    if (!textSearch){
-        res.status(400).json({error:400,messageError:"textSearch no existe"})
+    if (!textSearch) {
+        res.status(400).json({ error: 400, messageError: "textSearch no existe" })
     }
-    if (!limitImages||limitImages<3||limitImages>200){
-        res.status(400).json({error:400,messageError:"limitImages no existe o es menor que 3 o mayor que 200"})
+    if (!limitImages || limitImages < 3 || limitImages > 200) {
+        res.status(400).json({ error: 400, messageError: "limitImages no existe o es menor que 3 o mayor que 200" })
     }
-    
+
     let urlSearch = `https://pixabay.com/api/?key=12664246-2d4d93d2fe8f4ce834876a53d&q=${textSearch}&image_type=photo&per_page=${limitImages}`
     const resultImages = await getImage(urlSearch)
     let responseImages = {
@@ -25,7 +25,9 @@ app.get('/image/list', async function (req, res) {
     }
     let status = 200
     if (!resultImages.error) {
-        responseImages.images = resultImages.hits
+        resultImages.hits.map((hit) => {
+            responseImages.images.push(hit.largeImageURL)
+        })
         console.log(responseImages)
     } else {
         status = resultImages.error
